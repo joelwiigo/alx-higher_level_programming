71ccdbfe0eddf67328b15ab6a3c5f7c572eab96f
LWIIGO JOEL <105353353+joelwiigo@users.noreply.github.com>
2022-09-21 01:06:29 +0300

    Update 100-print_tebahphla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahphla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahphla.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-for ascii_num in range(122, 96, -1):
-	if ascii_num % 2 == 1:
-		ascii_num = ascii_num - 32
-	print("{:c}".format(ascii_num), end='')
+for i in range(122, 96, -1):
+    print("{:c}".format(i - 32) if i % 2 != 0 else "{:c}".format(i), end="")
